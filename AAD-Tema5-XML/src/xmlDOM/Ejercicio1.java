c9341011779982cf3408d3877133516587c3f02f
Jerónimo Vicente <contact@jeronimovicente.com>
2024-11-08 10:01:10 +0100

    commit
    Reorganizacion + Actualizacion ejercicio XML.

--- AAD-Tema5-XML/src/xmlDOM/Ejercicio1.java
+++ AAD-Tema5-XML/src/xmlDOM/Ejercicio1.java
@@ -4,56 +4,38 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import java.io.File;
 
 /*
-  	Deberás crear una aplicación Java que permita obtener toda la información contenida en el XML adjunto.
-	Ten en cuenta que deberán mostrarse por pantalla todos los datos referentes a los atributos y elementos del fichero.
+   Deberás crear una aplicación Java que permita obtener toda la información contenida en el XML adjunto.
+   Ten en cuenta que deberán mostrarse por pantalla todos los datos referentes a los atributos y elementos del fichero.
  */
 
 public class Ejercicio1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        try {
+            File archivoXML = new File("coches.xml");
 
-		try {
-			
-			File archXML = new File("coches.xml");
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(archXML);
-			doc.getDocumentElement().normalize();
-			
-			Element elemraiz = doc.getDocumentElement();
-            System.out.println("Elemento raíz: " + elemraiz.getNodeName());
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(archivoXML);
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Elemento raíz: " + doc.getDocumentElement().getNodeName());
+
             NodeList listaCoches = doc.getElementsByTagName("coche");
-			
-            for(int i = 0; i < listaCoches.getLength(); i++ ) {
-            	
-            	Node nodoCoche = listaCoches.item(i);
-           
-            	if (nodoCoche.getNodeType() == Node.ELEMENT_NODE) {
-            		Element elemCoche = (Element) nodoCoche;
-            		String idCoche = elemCoche.getAttribute("id");
-                    System.out.println("\nCoche ID: " + idCoche);
-
-                    NodeList listaElementos = elemCoche.getChildNodes();
-                    for (int j = 0; j < listaElementos.getLength(); j++) {
-                        Node nodoHijo = listaElementos.item(j);
-
-                        if (nodoHijo.getNodeType() == Node.ELEMENT_NODE) {
-                            Element elementoHijo = (Element) nodoHijo;
-                            System.out.println(elementoHijo.getTagName() + ": " + elementoHijo.getTextContent());
-                        }
-                    }
-            	}
+            for (int i = 0; i < listaCoches.getLength(); i++) {
+                Element coche = (Element) listaCoches.item(i);
+                System.out.println("\nCoche ID: " + coche.getAttribute("id"));
+                System.out.println("Marca: " + coche.getElementsByTagName("marca").item(0).getTextContent());
+                System.out.println("Modelo: " + coche.getElementsByTagName("modelo").item(0).getTextContent());
+                System.out.println("Cilindrada: " + coche.getElementsByTagName("cilindrada").item(0).getTextContent());
             }
-		} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-	}
-
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
